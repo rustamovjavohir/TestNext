09f91041a2426240627bf197ec3b9c17ad40a130
rustamovjavohir <rustamovj366@gmail.com>
2022-07-22 13:14:42 +0500

    remove db

--- TestNext/settings.py
+++ TestNext/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'shrine',
 
     'rest_framework',
+    'corsheaders',
 
 ]
 
@@ -50,6 +51,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
